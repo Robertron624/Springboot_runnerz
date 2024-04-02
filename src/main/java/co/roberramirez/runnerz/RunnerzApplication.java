95be929eb9fb5f338e6d8a1ba538209640969f4a
Roberto Ramirez <robert.624@hotmail.com>
2024-04-02 15:56:02 -0500

    Creating a User http client for obtaining user data from an external REST API to experiment how to connect to external APIs

--- src/main/java/co/roberramirez/runnerz/RunnerzApplication.java
+++ src/main/java/co/roberramirez/runnerz/RunnerzApplication.java
@@ -3,14 +3,21 @@ package co.roberramirez.runnerz;
 import co.roberramirez.runnerz.run.Location;
 import co.roberramirez.runnerz.run.Run;
 import co.roberramirez.runnerz.run.RunRepository;
+import co.roberramirez.runnerz.user.User;
+import co.roberramirez.runnerz.user.UserHttpClient;
+import co.roberramirez.runnerz.user.UserResClient;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class RunnerzApplication {
@@ -20,5 +27,22 @@ public class RunnerzApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(RunnerzApplication.class, args);
+    }
+
+    @Bean
+    UserHttpClient userHttpClient() {
+        RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+        return factory.createClient(UserHttpClient.class);
+    }
+    @Bean
+    CommandLineRunner runner (UserHttpClient client) {
+        return args -> {
+            List<User> users = client.findAll();
+            System.out.println("Users: " + users);
+
+            User user = client.findById(1);
+            System.out.println("User with Id 1: " + user);
+        };
     }
 }
---
+++ src/main/java/co/roberramirez/runnerz/user/Geo.java
@@ -0,0 +1,7 @@
+package co.roberramirez.runnerz.user;
+
+public record Geo(
+        String lat,
+        String lng
+) {
+}
